b2c6767a55210025fc0b8fa588a38148f1dbc1c4
Jon Wenger <jonwenger783@gmail.com>
2018-06-18 21:17:02 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -4,5 +4,5 @@ party_name="Halloween Party"
 party_date="October 31"
 party_time="6pm"
 host_name= "Harry Potter"
-puts "Dear #{guest_name} you are invited to the best #{party_name} ever on #{party_date} at #{party_time} sinserly"
+puts "Dear #{guest_name} you are invited to the best #{party_name} ever on #{party_date} at #{party_time} sincerely #{host_name}"
 # Try starting out with puts'ing a string.
