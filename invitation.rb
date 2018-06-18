8c6fccc96449509ee15f6c39df380c7ec2c1e4b9
Jon Wenger <jonwenger783@gmail.com>
2018-06-18 21:27:43 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,8 +1,3 @@
-# Code your prompts here!
-guest_name="voldermort"
-party_name="Halloween Party"
-party_date="October 31"
-party_time="6pm"
-host_name= "Harry Potter"
+def invitation 
 puts "Dear #{guest_name} you are invited to the best #{party_name} ever on #{party_date} at #{party_time} sincerely #{host_name}"
 # Try starting out with puts'ing a string.
